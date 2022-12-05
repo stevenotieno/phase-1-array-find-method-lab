2bf3347704f35e63d70b2552d0df7f81fc7a6b71
+ <+>
2022-12-05 16:33:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+var superbowlWin = superbowlWinRecord => {
+	let superbowlWon = superbowlWinRecord.find( superbowl => {  return superbowl.result === "W" })
+	if (superbowlWon){ 
+		return superbowlWon.year
+	} else {
+		return undefined
+	}
+}
+Footer
